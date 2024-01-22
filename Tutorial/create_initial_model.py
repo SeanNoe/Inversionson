a3cb6862ee425d2527d251732c8c90d45f8d45ed
SeanNoe <sebastian.noe@erdw.ethz.ch>
2024-01-22 11:28:00 +0100

    Add initial model other than 1d.

--- Tutorial/create_initial_model.py
+++ Tutorial/create_initial_model.py
@@ -1,9 +1,12 @@
 from salvus.mesh import simple_mesh
 import toml
+from salvus.mesh.tools.transforms import interpolate_mesh_to_mesh
+from salvus.namespace import UnstructuredMesh
 
+one_d = True
 
 # Load in min period from LASIF config, this drives the resolution of the mesh
-with open('LASIF_PROJECT/lasif_config.toml', 'r') as file:
+with open('INVERSIONSON_PROJECT_130s/LASIF_PROJECT/lasif_config.toml', 'r') as file:
     data = toml.load(file)
 
 period = data['simulation_settings']['minimum_period_in_s']
@@ -19,5 +22,20 @@ m.basic.elements_per_wavelength = 2.0
 
 mesh = m.create_mesh()
 
-# Save the mesh in the LASIF project
-mesh.write_h5('LASIF_PROJECT/MODELS/initial_model.h5')
+if one_d:
+    # Save the mesh in the LASIF project
+    mesh.write_h5('INVERSIONSON_PROJECT_130s/LASIF_PROJECT/MODELS/initial_model.h5')
+
+else:
+    mesh_load = UnstructuredMesh.from_h5('INVERSIONSON_PROJECT/OPTIMIZATION/MODELS/x_00025_Radius_57735.9056944908.h5')
+    mesh_interp = interpolate_mesh_to_mesh(
+        mesh_load, 
+        mesh, 
+        use_layers=True, 
+        use_1d_vertical_coordinate=True, 
+        fields_to_interpolate = ['VSV', "VSH","VPV","VPH","RHO"]
+    )
+    mesh_interp.write_h5('INVERSIONSON_PROJECT_130s/LASIF_PROJECT/MODELS/initial_model.h5')
+
+
+
